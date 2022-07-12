e91db90d38d4f1ca6ccdd1b4a967677fc91254cc
Jeff Zhang <zjffdu@apache.org>
2022-07-12 08:49:35 +0800

    [ZEPPELIN-5755] Support Spark 3.3 (#4388)
    * [ZEPPELIN-5755] Support Spark 3.3
    
    * use hadoop3 profile for spark 3.3

--- spark/interpreter/src/main/java/org/apache/zeppelin/spark/PySparkInterpreter.java
+++ spark/interpreter/src/main/java/org/apache/zeppelin/spark/PySparkInterpreter.java
@@ -233,11 +233,13 @@ public class PySparkInterpreter extends PythonInterpreter {
     }
   }
 
-  public boolean isSpark1() {
-    return sparkInterpreter.getSparkVersion().getMajorVersion() == 1;
-  }
-
+  // Used by PySpark
   public boolean isSpark3() {
     return sparkInterpreter.getSparkVersion().getMajorVersion() == 3;
+  }
+
+  // Used by PySpark
+  public boolean isAfterSpark33() {
+    return sparkInterpreter.getSparkVersion().newerThanEquals(SparkVersion.SPARK_3_3_0);
   }
 }
